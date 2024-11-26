a4eab426c191e9b8998a1e63729c974c92ba9805
enricolam <32127499+enricolam@users.noreply.github.com>
2024-11-27 02:28:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Enable navigation prompt
+window.onbeforeunload = function() {
+    return true;
+};
+// Remove navigation prompt
+window.onbeforeunload = null;
 /*!
  * modified qr.js -- QR code generator in Javascript (revision 2011-01-19)
  * Written by Kang Seonghoon <public+qrjs@mearie.org>.
